a2e6541bc01dbba9aa22a39712c6dc0f98c8fb89
Kevin Wang <kwang01@college.harvard.edu>
2016-03-10 00:19:55 -0500

    changes

--- clocks/clocks.py
+++ clocks/clocks.py
@@ -1,47 +1,72 @@
-from multiprocessing import Process, Pipe
+import multiprocessing
+from multiprocessing import Process
 from collections import deque
 import random
 import sys
+from datetime import datetime
 import time
+import Queue
 
 
-def f(tofirst, tosecond, outfile):
+def f(own, first, second, outfile):
     rate = random.randint(1, 6)
-    sleep_interval = 1 / float(rate)
-    print(sleep_interval)
-    sys.stdout.flush()
+    # how long each tick should be
+    tick_interval = 1 / float(rate)
+    # local queue
     q = deque()
+    # the all important logical clock!
     logical_clock = 0
+
+    ownname, ownq = own
+    firstname, tofirst = first
+    secondname, tosecond = second
     with open(outfile, 'w') as f:
+        # loop
         while(True):
-            start_time = time.time()
-            while(tofirst.poll()):
-                q.append(tofirst.recv())
-            while(tosecond.poll()):
-                q.append(tosecond.recv())
+            # block for tick interval amount of time overall
+            time_left = tick_interval
+            while(time_left > 0):
+                try:
+                    start = time.time()
+                    new_elt = ownq.get(True, time_left)
+                    q.append(new_elt)
+                    end = time.time()
+                    time_left -= (end - start)
+                except Queue.Empty:
+                    break
+            to_print = ""
             if(len(q) == 0):
                 randgen = random.randint(1, 10)
+                logical_clock += 1
                 if randgen == 1:
-                    tofirst.send(logical_clock)
+                    tofirst.put((ownname, logical_clock))
+                    to_print += ("Sent message to: " + firstname)                    
                 elif randgen == 2:
-                    tosecond.send(logical_clock)
+                    tosecond.put((ownname, logical_clock))
+                    to_print += ("Sent message to: " + secondname)
                 elif randgen == 3:
-                    tofirst.send(logical_clock)
-                    tosecond.send(logical_clock)
+                    tofirst.put((ownname, logical_clock))
+                    tosecond.put((ownname, logical_clock))
+                    to_print += "Send message to: both"
                 else:
-                    logical_clock += 1
+                    to_print += "Internal Event"
             else:
-                message = q.popleft()
-                logical_clock = max(message, logical_clock + 1)
-                f.write(str(message) + str(time.time()) + str(len(q)) + str(logical_clock))
+                pname, message = q.popleft()
+                logical_clock = max(message, logical_clock) + 1
+                to_print += "Received message: " + str(message) + " from: " + str(pname) \
+                            + "with remaining queue size: " + str(len(q))
+            to_print += " at logical time: " + str(logical_clock) + " and system time " \
+                        + str(datetime.now())
+            print(to_print)
 
 def main():
-    pipe12, pipe21 = Pipe()
-    pipe13, pipe31 = Pipe()
-    pipe23, pipe32 = Pipe()
-    p1 = Process(target = f, args = (pipe12, pipe13, "p1.txt"))
-    p2 = Process(target = f, args = (pipe23, pipe21, "p2.txt"))
-    p3 = Process(target = f, args = (pipe31, pipe32, "p3.txt"))
+    first = ("p1", multiprocessing.Queue())
+    second = ("p2", multiprocessing.Queue())
+    third = ("p3", multiprocessing.Queue())
+    
+    p1 = Process(target = f, args = (first, second, third, "p1.txt"))
+    p2 = Process(target = f, args = (second, third, first, "p2.txt"))
+    p3 = Process(target = f, args = (third, first, second, "p3.txt"))
     p1.start()
     p2.start()
     p3.start()
